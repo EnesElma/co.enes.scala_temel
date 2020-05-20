1d364c0c5756b373a146adacb179e51da899cc92
EnesElma <elmaenes123@gmail.com>
2020-05-20 19:50:02 +0300

    Spark mongo connet and read from mongodb

---
+++ src/main/scala/sparkTemel/dataframeDataset/ReadFromMongoDB.scala
@@ -0,0 +1,35 @@
+package sparkTemel.dataframeDataset
+
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.ReadConfig
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+
+
+object ReadFromMongoDB {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark=SparkSession.builder()
+      .appName("ReadFromMongpDB")
+      .master("local")
+      .config("spark.mongodb.input.uri","mongodb://localhost:27017/mylib.test")
+      .config("spark.mongodb.output.uri","mongodb://localhost:27017/mylib.test")
+      .getOrCreate()
+
+
+    //Read from mongoDB:
+
+    val df2 = MongoSpark.load(spark)
+    df2.show()
+
+
+
+
+
+
+
+  }
+}
